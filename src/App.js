d1d9afae7a530cf0ef477829e4746a2423c1f0dd
Henrique Ritter <henrique-ritter@hotmail.com>
2020-04-25 13:14:40 -0300

    challenge finished

--- src/App.js
+++ src/App.js
@@ -8,16 +8,15 @@ function App() {
 
   useEffect(() => {
     api.get('/repositories').then(response => {
-      setRepositories(response.data);
-      console.log(response.data);
+      setRepositories(response.data); //insert repositories in repositories array
     })
   },[]);
 
   async function handleAddRepository() {
     const response = await api.post('/repositories', {
-      title: 'nome Repo',
-      url: 'http://github',
-      techs: ['Node', 'React', 'Native']
+      title: 'novo Repo',
+      url: 'http://github.com/henriqueritter/novo-repo',
+      techs: ['Node.js', 'ReactJS', 'React Native']
     });
     const repo = response.data;
     setRepositories([...repositories, repo]);
@@ -26,7 +25,9 @@ function App() {
   async function handleRemoveRepository(id) {
     await api.delete(`/repositories/${id}`);
     
+    //remove the deleted repositorie from repositories array
     const repoAfterDelete = repositories.filter(repo=>repo.id !== id)
+    //update repositories array without the deleted repo
     setRepositories(repoAfterDelete);
   }
 
@@ -37,7 +38,6 @@ function App() {
       {repositories.map(repo =>{
         return <li key={repo.id}>
           {repo.title}
-
           <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
